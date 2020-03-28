360bdc5ce35e0212fb297358df2cf3821944b530
untitle <53917645+under-web@users.noreply.github.com>
2020-03-29 01:06:14 +0300

    Update coronavirus.py

--- coronavirus.py
+++ coronavirus.py
@@ -6,8 +6,16 @@ r = requests.get(url)
 soup = BeautifulSoup(r.text, 'lxml')
 hazard = soup.find('div', class_='total_block col-lg-2 col-sm-4 col-xs-6 offset-md-1').find('span', class_='num').text
 hazard = hazard.split('+')
-print(' Всего зараженных - ', hazard[0],'\n', 'Прибавилось за сутки +', hazard[1])
+try:
+	print(' Всего зараженных - ', hazard[0])
+except:
+	pass
+try:
+	print('Прибавилось - ', hazard[1])
+except:
+	pass	
 activ = soup.find_all('div', class_='total_block col-lg-2 col-sm-4 col-xs-6')
 for i in activ:
 	tx = i.text
 	print(tx)
+
